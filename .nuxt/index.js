bba08425731253f9a0c54657dc5a2366649c72c7
tigran <tigr.kman@gmail.com>
2022-05-02 23:13:53 +0300

    Добавление компонентов с автоопределяющимися импортами на главную страницу

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,6 +12,8 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
+import nuxt_plugin_plugin_6f589ac8 from 'nuxt_plugin_plugin_6f589ac8' // Source: .\\components\\plugin.js (mode: 'all')
+
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
 
@@ -173,6 +175,10 @@ async function createApp(ssrContext, config = {}) {
     }
   }
   // Plugin execution
+
+  if (typeof nuxt_plugin_plugin_6f589ac8 === 'function') {
+    await nuxt_plugin_plugin_6f589ac8(app.context, inject)
+  }
 
   // Lock enablePreview in context
   if (process.static && process.client) {
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <div v-for="home in HomeData" :key="home.objectID" style="float: left; margin: 10px">
+      <LazyHomeCard :home="home" />
+    </div>
+  </div>
 </template>
+
+<script>
+import HomeData from '~/data/homes'
+
+export default {
+  data() {
+    return {
+      HomeData: HomeData.slice(0, 3)
+    }
+  }
+}
+</script>
